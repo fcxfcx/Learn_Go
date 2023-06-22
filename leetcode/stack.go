c4a6018a65ac451fc91d8394dd81a29247f90c16
方楚星 <fcxfcx@bupt.edu.cn>
2023-06-22 13:42:08 +0800

    Revert "Update stack.go"
    This reverts commit 53a07bad8e41abe6ad09fd6cd6a9adefd9e13fcd.

--- leetcode/stack.go
+++ leetcode/stack.go
@@ -1,8 +1,6 @@
 package leetcode
 
-import (
-	"strings"
-)
+import "strings"
 
 // 有效的括号
 func IsValidBracket(s string) bool {
@@ -37,47 +35,10 @@ func SimplifyPath(path string) string {
 			if len(stack) > 0 {
 				stack = stack[:len(stack)-1]
 			}
+
 		} else if item != "" && item != "." {
 			stack = append(stack, item)
 		}
 	}
 	return "/" + strings.Join(stack, "/")
-}
-
-// 最小栈
-type MinStack struct {
-	stack    []int
-	minStack []int
-}
-
-func StackConstructor() MinStack {
-	return MinStack{
-		stack:    make([]int, 0),
-		minStack: make([]int, 0),
-	}
-}
-
-func (ms *MinStack) Push(val int) {
-	ms.stack = append(ms.stack, val)
-	if len(ms.minStack) == 0 {
-		ms.minStack = append(ms.minStack, val)
-	} else if ms.GetMin() < val {
-		ms.minStack = append(ms.minStack, ms.GetMin())
-	} else {
-		ms.minStack = append(ms.minStack, val)
-	}
-
-}
-
-func (ms *MinStack) Pop() {
-	ms.stack = ms.stack[:len(ms.stack)-1]
-	ms.minStack = ms.minStack[:len(ms.minStack)-1]
-}
-
-func (ms *MinStack) Top() int {
-	return ms.stack[len(ms.stack)-1]
-}
-
-func (ms *MinStack) GetMin() int {
-	return ms.minStack[len(ms.minStack)-1]
 }
