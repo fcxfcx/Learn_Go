5e4934197345514c9b0428f703c852fbbcab0979
方楚星 <fcxfcx@bupt.edu.cn>
2023-08-10 17:09:49 +0800

    日常刷题

--- leetcode/map.go
+++ leetcode/map.go
@@ -26,3 +26,36 @@ func NumIslands(grid [][]byte) int {
 	}
 	return count
 }
+
+// 被围绕的区域
+func Solve(board [][]byte) {
+	height, width := len(board), len(board[0])
+	var dfs func(board [][]byte, i int, j int)
+	dfs = func(board [][]byte, i, j int) {
+		if i < 0 || i >= height || j < 0 || j >= width || board[i][j] != 'O' {
+			return
+		}
+		board[i][j] = '#'
+		dfs(board, i+1, j)
+		dfs(board, i-1, j)
+		dfs(board, i, j+1)
+		dfs(board, i, j-1)
+	}
+	for i := 0; i < height; i++ {
+		dfs(board, i, 0)
+		dfs(board, i, width-1)
+	}
+	for j := 0; j < width; j++ {
+		dfs(board, 0, j)
+		dfs(board, height-1, j)
+	}
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			if board[i][j] == '#' {
+				board[i][j] = 'O'
+			} else if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			}
+		}
+	}
+}
