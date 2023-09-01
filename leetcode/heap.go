56f710c8f0e43c2d5437ad8277f3ad6a4d707802
方楚星 <fcxfcx@bupt.edu.cn>
2023-09-01 11:17:15 +0800

    日常刷题

--- leetcode/heap.go
+++ leetcode/heap.go
@@ -1,6 +1,10 @@
 package leetcode
 
-import "fmt"
+import (
+	"container/heap"
+	"fmt"
+	"sort"
+)
 
 // 使用完全二叉树实现最小堆，不依靠Go语言自己的接口
 type MinHeap struct {
@@ -8,10 +12,12 @@ type MinHeap struct {
 }
 
 func NewMinHeap() *MinHeap {
+	// 构造方法
 	return &MinHeap{}
 }
 
 func (h *MinHeap) Push(val int) {
+	// 新数字加入到末尾，然后上浮
 	h.arr = append(h.arr, val)
 	h.heapifyUp(len(h.arr) - 1)
 }
@@ -24,9 +30,11 @@ func (h *MinHeap) Pop() int {
 	root := h.arr[0]
 	last := len(h.arr) - 1
 
+	// 首尾交换之后删除尾部
 	h.arr[0] = h.arr[last]
 	h.arr = h.arr[:last]
 
+	// 交换到首部的需要向下沉
 	if len(h.arr) > 0 {
 		h.heapifyDown(0)
 	}
@@ -35,9 +43,12 @@ func (h *MinHeap) Pop() int {
 }
 
 func (h *MinHeap) heapifyUp(index int) {
+	// 对index点处的数进行上浮操作
 	parent := (index - 1) / 2
 
 	for index > 0 && h.arr[index] < h.arr[parent] {
+		// 当index处不满足最小堆要求的时候
+		// 交换index节点和父节点
 		h.arr[index], h.arr[parent] = h.arr[parent], h.arr[index]
 		index = parent
 		parent = (index - 1) / 2
@@ -57,6 +68,7 @@ func (h *MinHeap) heapifyDown(index int) {
 			smallest = right
 		}
 		if smallest == index {
+			// 如果当前索引的数就是最小的了，则流程停止
 			break
 		}
 
@@ -76,4 +88,47 @@ func FindKthLargest(nums []int, k int) int {
 		fmt.Println(hp)
 	}
 	return hp.Pop()
+}
+
+// 使用最大堆的方法
+// 此处使用的是go语言自带的heap.Interface接口来实现
+type MaxHeap struct{ sort.IntSlice }
+
+func (hp MaxHeap) Len() int            { return len(hp.IntSlice) }
+func (hp MaxHeap) Less(i, j int) bool  { return hp.IntSlice[i] > hp.IntSlice[j] }
+func (hp MaxHeap) Swap(i, j int)       { hp.IntSlice[i], hp.IntSlice[j] = hp.IntSlice[j], hp.IntSlice[i] }
+func (hp *MaxHeap) Push(x interface{}) { hp.IntSlice = append(hp.IntSlice, x.(int)) }
+func (hp *MaxHeap) Pop() interface{} {
+	n := len(hp.IntSlice)
+	result := (hp.IntSlice)[n-1]
+	hp.IntSlice = hp.IntSlice[:n-1]
+	return result
+}
+
+// IPO
+func findMaximizedCapital(k int, w int, profits []int, capital []int) int {
+	n := len(profits)
+	type pair struct{ c, p int }
+	arr := make([]pair, n)
+	for i, profit := range profits {
+		arr[i] = pair{capital[i], profit}
+	}
+	// 将项目按照所需资金从低到高排序
+	sort.Slice(arr, func(i, j int) bool { return arr[i].c < arr[j].c })
+	// 使用最大堆
+	hp := &MaxHeap{}
+	for cur := 0; k > 0; k-- {
+		for cur < n && arr[cur].c <= w {
+			// 将当前资本所支持的项目加入堆中
+			// 堆中按利润排序
+			heap.Push(hp, arr[cur].p)
+			cur++
+		}
+		if hp.Len() == 0 {
+			// 堆为空说明无项目可做了，直接退出
+			break
+		}
+		w += heap.Pop(hp).(int)
+	}
+	return w
 }
