a1344f775621c5754f8fef866d75f40d9d026b40
方楚星 <fcxfcx@bupt.edu.cn>
2023-09-07 10:53:29 +0800

    日常刷题

--- leetcode/bit_manipulation.go
+++ leetcode/bit_manipulation.go
@@ -52,3 +52,45 @@ func HammingWeight(num uint32) (count int) {
 	}
 	return
 }
+
+// 只出现一次的数字
+func SingleNumber(nums []int) (res int) {
+	// 交换律：a ^ b ^ c <=> a ^ c ^ b
+	// 任何数于0异或为任何数 0 ^ n => n
+	// 相同的数异或为0: n ^ n => 0
+	for _, num := range nums {
+		res = res ^ num
+	}
+	return
+}
+
+// 只出现一次的数字Ⅱ
+func SingleNumberPlus(nums []int) int {
+	// 对于数组中非答案的元素，每一个元素都出现了3次，对应着第i个二进制位的3个0或3个1
+	// 无论是哪一种情况，它们的和都是3的倍数（即和为 0 或 3）
+	// 答案的第 i 个二进制位就是数组中所有元素的第 i 个二进制位之和除以 3 的余数。
+	ans := int32(0)
+	for i := 0; i < 32; i++ {
+		total := int32(0)
+		for _, num := range nums {
+			total += int32(num) >> i & 1
+		}
+		if total%3 > 0 {
+			ans |= 1 << i
+		}
+	}
+	return int(ans)
+}
+
+// 数字范围按位与
+func RangeBitwiseAnd(left int, right int) int {
+	// 对所有数字执行按位与运算的结果是所有对应二进制字符串的公共前缀再用零补上后面的剩余位
+	// 所有这些二进制字符串的公共前缀也即指定范围的起始和结束数字left和right的公共前缀
+	// 因此就找left和right的公共前缀
+	shift := 0
+	for left < right {
+		left, right = left>>1, right>>1
+		shift++
+	}
+	return left << shift
+}
