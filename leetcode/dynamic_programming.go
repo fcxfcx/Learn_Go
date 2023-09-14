0dd0875191e9be6cb33dddd72420ddd7ea6afaff
方楚星 <fcxfcx@bupt.edu.cn>
2023-09-14 10:49:11 +0800

    日常刷题

--- leetcode/dynamic_programming.go
+++ leetcode/dynamic_programming.go
@@ -1,7 +1,9 @@
 package leetcode
 
+import "math"
+
 // 爬楼梯
-func climbStairs(n int) int {
+func ClimbStairs(n int) int {
 	if n <= 2 {
 		return n
 	}
@@ -16,7 +18,7 @@ func climbStairs(n int) int {
 }
 
 // 打家劫舍
-func rob(nums []int) int {
+func Rob(nums []int) int {
 	n := len(nums)
 	if n == 1 {
 		return nums[0]
@@ -29,4 +31,57 @@ func rob(nums []int) int {
 		a, b = b, temp
 	}
 	return temp
+}
+
+// 单词拆分
+func WordBreak(s string, wordDict []string) bool {
+	dic := make(map[string]bool, 0)
+	maxWord := 0
+	for _, word := range wordDict {
+		if len(word) > maxWord {
+			// 顺便维护一下词典里最长的词是多少
+			maxWord = len(word)
+		}
+		dic[word] = true
+	}
+	// dp[i]为true代表i之前的都可以组成
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			if i-j > maxWord {
+				continue
+			}
+			if dp[j] && dic[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[len(s)]
+}
+
+// 零钱兑换
+func CoinChange(coins []int, amount int) int {
+	//dp[i]代表面值i以前的钱最少兑换的硬币数
+	dp := make([]int, amount+1)
+	for i := range dp {
+		// 方便之后判断最小值，除以二是为了避免+1后溢出
+		dp[i] = math.MaxInt / 2
+	}
+	dp[0] = 0
+	for i := 1; i <= amount; i++ {
+		for j := 0; j < len(coins); j++ {
+			// 枚举最后一枚硬币的面值
+			target := i - coins[j]
+			if target < 0 {
+				continue
+			}
+			dp[i] = min(dp[i], dp[target]+1)
+		}
+	}
+	if dp[amount] < math.MaxInt/2 {
+		return dp[amount]
+	}
+	return -1
 }
