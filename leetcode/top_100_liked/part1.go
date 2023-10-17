297aef5362e4d3b5e54f504f74d5d1a05f97ab83
方楚星 <50919312+fcxfcx@users.noreply.github.com>
2023-10-17 21:22:49 +0800

    日常刷题

--- leetcode/top_100_liked/part1.go
+++ leetcode/top_100_liked/part1.go
@@ -420,3 +420,49 @@ func firstMissingPositive(nums []int) int {
 	}
 	return n + 1
 }
+
+// 矩阵置零
+func SetZeroes(matrix [][]int) {
+	m, n := len(matrix), len(matrix[0])
+	// 储存第一行和第一列是否原来就含有零
+	column, row := false, false
+	for i := 0; i < m; i++ {
+		if matrix[i][0] == 0 {
+			// 发现第一列中有一个零，则第一列记录为全零
+			column = true
+			break
+		}
+	}
+	for i := 0; i < n; i++ {
+		if matrix[0][i] == 0 {
+			// 发现第一行中有一个零，则第一行记录为全零
+			row = true
+			break
+		}
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if matrix[i][j] == 0 {
+				matrix[i][0] = 0
+				matrix[0][j] = 0
+			}
+		}
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if matrix[i][0] == 0 || matrix[0][j] == 0 {
+				matrix[i][j] = 0
+			}
+		}
+	}
+	if row {
+		for j := 0; j < n; j++ {
+			matrix[0][j] = 0
+		}
+	}
+	if column {
+		for i := 0; i < m; i++ {
+			matrix[i][0] = 0
+		}
+	}
+}
