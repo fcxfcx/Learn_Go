32acadc4001b92331b97f3f2f117b22c408949fd
方楚星 <1098311731@qq.com>
2023-11-26 11:09:17 +0800

    训练营day6

--- leetcode_master/linklist.go
+++ leetcode_master/linklist.go
@@ -158,3 +158,27 @@ func GetIntersectionNode(headA, headB *ListNode) *ListNode {
 	}
 	return pA
 }
+
+// No.142 环形链表 II
+func DetectCycle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	fast, slow := head, head
+	for fast != nil {
+		slow = slow.Next
+		if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		if fast == slow {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
