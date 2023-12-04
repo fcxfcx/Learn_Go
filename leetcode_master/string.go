44e03d4c9943d9a33fc79b0201708e3a0cb14323
方楚星 <fcxfcx@bupt.edu.cn>
2023-12-04 17:27:06 +0800

    训练营day12

--- leetcode_master/string.go
+++ leetcode_master/string.go
@@ -14,18 +14,54 @@ func ReverseString(s []byte) {
 // No.541 反转字符串Ⅱ
 func ReverseStr(s string, k int) string {
 	str := []byte(s)
-	count := len(s) / (2 * k)
-	for i := 0; i < count; i++ {
-		start := i * k
-		end := start + k
-		if end >= len(s) {
-			return string(str)
+	for i := 0; i < len(s); i += 2 * k {
+		if i+k <= len(s) {
+			ReverseString(str[i : i+k])
+		} else {
+			ReverseString(str[i:len(s)])
 		}
-		for start < end {
-			str[start], str[end] = str[end], str[start]
-			start++
-			end--
+	}
+	return string(str)
+}
+
+// No.151 反转字符串中的单词
+func ReverseWords(s string) string {
+	str := []byte(s)
+	n := len(s)
+	// 双指针法去除多余空格
+	slow, fast := 0, 0
+	// 去除头部多余空格
+	for fast < n && str[fast] == ' ' {
+		fast++
+	}
+	// 去除中间多余空格
+	for ; fast < n; fast++ {
+		if fast > 1 && str[fast-1] == str[fast] && str[fast] == ' ' {
+			// 如果出现连续空格，右移快指针，这里判定的是出现两个及以上的空格，只记录第一个空格给slow
+			continue
 		}
+		str[slow] = str[fast]
+		slow++
+	}
+	// 删除尾部空格
+	if slow > 1 && str[slow-1] == ' ' {
+		str = str[:slow-1]
+	} else {
+		str = str[:slow]
+	}
+
+	// 反转整个字符串
+	ReverseString(str)
+
+	// 反转每个单词
+	i := 0
+	for i < len(str) {
+		j := i
+		for ; j < len(str) && str[j] != ' '; j++ {
+		}
+		ReverseString(str[i:j])
+		// j此时对应的是空格，所以还要加一
+		i = j + 1
 	}
 	return string(str)
 }
