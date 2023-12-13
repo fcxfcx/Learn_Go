ce67aadaefcaa07f99268832c54263f6d3413348
方楚星 <fcxfcx@bupt.edu.cn>
2023-12-13 10:54:00 +0800

    训练营day19

--- leetcode_master/binary_tree.go
+++ leetcode_master/binary_tree.go
@@ -6,6 +6,7 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// No.102 二叉树的层序遍历
 func LevelOrder(root *TreeNode) (res [][]int) {
 	if root == nil {
 		return
@@ -31,6 +32,7 @@ func LevelOrder(root *TreeNode) (res [][]int) {
 	return
 }
 
+// No.107 二叉树的层序遍历Ⅱ
 func LevelOrderBottom(root *TreeNode) (res [][]int) {
 	if root == nil {
 		return
@@ -59,4 +61,61 @@ func LevelOrderBottom(root *TreeNode) (res [][]int) {
 func preInsertSlice(i []int, s [][]int) [][]int {
 	res := append([][]int{i}, s...)
 	return res
+}
+
+// No.111 二叉树的最小深度
+func MinDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	depth := 1
+	q := []*TreeNode{}
+	q = append(q, root)
+	for len(q) != 0 {
+		n := len(q)
+		for i := 0; i < n; i++ {
+			node := q[i]
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+			if node.Left == nil && node.Right == nil {
+				return depth
+			}
+		}
+		depth++
+		q = q[n:]
+	}
+	return depth
+}
+
+// No.226 翻转二叉树
+func InvertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left = InvertTree(root.Left)
+	root.Right = InvertTree(root.Right)
+	root.Left, root.Right = root.Right, root.Left
+	return root
+}
+
+// No.101 对称二叉树
+func IsSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return checkSymmetric(root.Left, root.Right)
+}
+
+func checkSymmetric(left *TreeNode, right *TreeNode) bool {
+	if left == nil || right == nil {
+		return left == right
+	}
+	if left.Val != right.Val {
+		return false
+	}
+	return checkSymmetric(left.Left, right.Right) && checkSymmetric(left.Right, right.Left)
 }
