1076365b42b9768f868cc99e49cd1c826668b561
方楚星 <fcxfcx@bupt.edu.cn>
2024-01-04 11:33:11 +0800

    训练营day34

--- leetcode_master/greedy.go
+++ leetcode_master/greedy.go
@@ -1,6 +1,9 @@
 package leetcode_master
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 // No.455 分发饼干
 func FindContentChildren(g []int, s []int) int {
@@ -18,4 +21,37 @@ func FindContentChildren(g []int, s []int) int {
 		}
 	}
 	return count
+}
+
+// No.376 摆动序列
+func WiggleMaxLength(nums []int) int {
+	// 两个节点的差值，记录前一个和当前的
+	preDiff, curDiff := 0, 0
+	// 默认最后一个数也是峰
+	result := 1
+	for i := 0; i < len(nums)-1; i++ {
+		curDiff = nums[i+1] - nums[i]
+		// 找到一个峰或谷
+		if (preDiff >= 0 && curDiff < 0) || (preDiff <= 0 && curDiff > 0) {
+			result++
+			preDiff = curDiff
+		}
+	}
+	return result
+}
+
+// No.53 最大子数组和
+func MaxSubArray(nums []int) int {
+	result := math.MinInt
+	curSum := 0
+	for _, num := range nums {
+		curSum += num
+		if curSum > result {
+			result = curSum
+		}
+		if curSum <= 0 {
+			curSum = 0
+		}
+	}
+	return result
 }
