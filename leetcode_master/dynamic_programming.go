badb6f384baede59c7a985a8d9cbcd6dac9bac2b
方楚星 <1098311731@qq.com>
2024-01-15 17:10:12 +0800

    训练营day41

--- leetcode_master/dynamic_programming.go
+++ leetcode_master/dynamic_programming.go
@@ -30,3 +30,31 @@ func ClimbStairs(n int) int {
 	}
 	return dp[1]
 }
+
+// No.746 使用最小花费爬楼梯
+func MinCostClimbingStairs(cost []int) int {
+	// 储存前面两级台阶最小花费
+	dp := [2]int{cost[0], cost[1]}
+	for i := 2; i < len(cost); i++ {
+		dp[0], dp[1] = dp[1], min(dp[0], dp[1])+cost[i]
+	}
+	return min(dp[1], dp[0])
+}
+
+// No.62 不同路径
+func UniquePaths(m int, n int) int {
+	// 储存当前行每个格子的走法
+	dp := make([]int, n)
+	for i := 0; i < m; i++ {
+		// 每一行的第一个格子一定只有一种走法
+		dp[0] = 1
+		for j := 1; j < n; j++ {
+			if i == 0 {
+				dp[j] = 1
+			} else {
+				dp[j] = dp[j-1] + dp[j]
+			}
+		}
+	}
+	return dp[n-1]
+}
