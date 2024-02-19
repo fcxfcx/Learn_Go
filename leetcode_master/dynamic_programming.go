bda61c208dc74e1bee19d89470e8732b9687e891
方楚星 <50919312+fcxfcx@users.noreply.github.com>
2024-02-19 15:10:17 +0800

    训练营day59

--- leetcode_master/dynamic_programming.go
+++ leetcode_master/dynamic_programming.go
@@ -476,11 +476,49 @@ func LongestCommonSubsequence(text1 string, text2 string) int {
 			if text1[i-1] == text2[j-1] {
 				dp[i][j] = dp[i-1][j-1] + 1
 			} else {
-				dp[i][j] = max(dp[i-2][j-1], dp[i-1][j-2])
+				dp[i][j] = max(dp[i][j-1], dp[i-1][j])
 			}
 			if dp[i][j] > result {
 				result = dp[i][j]
 			}
+		}
+	}
+	return result
+}
+
+// No.1035 不相交的线
+func MaxUncrossedLines(nums1 []int, nums2 []int) int {
+	m, n := len(nums1), len(nums2)
+	dp := make([][]int, m+1)
+	for i := 0; i < m+1; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	result := 0
+	for i := 1; i < m+1; i++ {
+		for j := 1; j < n+1; j++ {
+			if nums1[i-1] == nums2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+			if dp[i][j] > result {
+				result = dp[i][j]
+			}
+		}
+	}
+	return result
+}
+
+// No.53 最大子树和
+func MaxSubArrayDP(nums []int) int {
+	n := len(nums)
+	dp := make([]int, n)
+	dp[0] = nums[0]
+	result := nums[0]
+	for i := 1; i < n; i++ {
+		dp[i] = max(nums[i], dp[i-1]+nums[i])
+		if dp[i] > result {
+			result = dp[i]
 		}
 	}
 	return result
