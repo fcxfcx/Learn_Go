cec8de7ed1a2f878914326754265799ecfd6e900
方楚星 <50919312+fcxfcx@users.noreply.github.com>
2024-02-20 12:23:38 +0800

    训练营day60

--- leetcode_master/dynamic_programming.go
+++ leetcode_master/dynamic_programming.go
@@ -523,3 +523,49 @@ func MaxSubArrayDP(nums []int) int {
 	}
 	return result
 }
+
+// No.392 判断子序列
+func IsSubsequence(s string, t string) bool {
+	m, n := len(s), len(t)
+	dp := make([][]int, m+1)
+	for i := 0; i < m+1; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 1; i < m+1; i++ {
+		for j := 1; j < n+1; j++ {
+			if s[i-1] == t[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = dp[i][j-1]
+			}
+		}
+	}
+	return dp[m][n] == m
+}
+
+//No.115 不同的子序列
+func NumDistinct(s string, t string) int {
+	len_s, len_t := len(s), len(t)
+	// dp[i][j] 表示s到i-1处包含了多少个t到j-1处的子序列
+	dp := make([][]int, len_s+1)
+	for i := 0; i < len_s+1; i++ {
+		dp[i] = make([]int, len_t+1)
+	}
+	for i := 0; i < len_s+1; i++ {
+		// 对于j=0的情况，空字符串可以删除所有字符后匹配
+		dp[i][0] = 1
+	}
+	for i := 1; i < len_s+1; i++ {
+		for j := 1; j < len_t+1; j++ {
+			if s[i-1] == t[j-1] {
+				// 如果当前两个位置相同，则可以有两种选择：
+				// 1. 使用当前的位置，则有和前面i-1和j-1位置相同个数的选项
+				// 2. 不使用当前的位置，让s里面前一个位置去匹配
+				dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+	return dp[len_s][len_t]
+}
